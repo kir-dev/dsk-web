c8e348c65c59f79d06bb4bb09045bbd86da435fc
PuppiBarna <puppi.barna@gmail.com>
2026-03-19 23:10:06 +0100

    refactor: update gym reservation DTO and controller to use Reservation type instead of objects

--- apps/backend/src/module/gym-reservation/gym-reservation.controller.ts
+++ apps/backend/src/module/gym-reservation/gym-reservation.controller.ts
@@ -1,39 +1,43 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, Query } from '@nestjs/common';
 import { GymReservationService } from './gym-reservation.service';
 import { CreateGymReservationDto } from './dto/create-gym-reservation.dto';
 import { UpdateGymReservationDto } from './dto/update-gym-reservation.dto';
+import { Reservation } from 'PrismaGen/client';
 
 @Controller()
 export class GymReservationController {
   constructor(private readonly gymReservationService: GymReservationService) {}
 
   @Post()
-  async create(@Body() createGymReservationDto: CreateGymReservationDto): Promise<object> {
+  async create(@Body() createGymReservationDto: CreateGymReservationDto): Promise<Reservation> {
     return this.gymReservationService.create(createGymReservationDto);
   }
 
   @Get()
-  async findAll(): Promise<object[]> {
+  async findAll(): Promise<Reservation[]> {
     return this.gymReservationService.findAll();
   }
 
   @Get('by-date')
-  async findByTime(@Query() startTime: Date, @Query() endTime: Date): Promise<object> {
+  async findByTime(@Query() startTime: Date, @Query() endTime: Date): Promise<Reservation[]> {
     return this.gymReservationService.findAllByDate(startTime, endTime);
   }
 
   @Get(':userId')
-  async findAllByUserId(@Param('userId') userId: string): Promise<object[]> {
+  async findAllByUserId(@Param('userId', ParseUUIDPipe) userId: string): Promise<Reservation[]> {
     return this.gymReservationService.findAllByUserId(userId);
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateGymReservationDto: UpdateGymReservationDto): Promise<object> {
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateGymReservationDto: UpdateGymReservationDto
+  ): Promise<Reservation> {
     return this.gymReservationService.update(id, updateGymReservationDto);
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string): Promise<void> {
-    this.gymReservationService.delete(id);
+  async delete(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.gymReservationService.delete(id);
   }
 }
